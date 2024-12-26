055b361ab71c2d44181ef90465038ef82770ad95
KiiMYoo <heathen8119@naver.com>
2024-12-26 20:33:41 +0900

    초기 커밋.

---
+++ Animal/Animal.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal
+{
+
+public:
+
+	Animal()
+	{
+		cout <<"Animal 생성자 호출" << endl;
+	}
+
+	virtual void makesound() = 0;
+
+	~Animal()
+	{
+		cout << "Animal 소멸자 호출" << endl;
+	}
+
+};
+
+class Dog : public Animal
+{
+public:
+	Dog()
+	{
+		cout << "Dog 생성자 호출" << endl;
+	}
+
+	void makesound()
+	{
+		cout << "Dog: Woooooof!" << endl;
+	}
+
+	~Dog()
+	{
+		cout << "Dog 소멸자 호출" << endl;
+	}
+
+private:
+
+};
+
+class Cat : public Animal
+{
+public:
+	Cat()
+	{
+		cout << "Cat 생성자 호출" << endl;
+	}
+
+	void makesound()
+	{
+		cout << "Cat: Meowoooow!" << endl;
+	}
+
+	~Cat()
+	{
+		cout << "Cat 소멸자 호출" << endl;
+	}
+
+private:
+
+};
+
+class Cow : public Animal
+{
+public:
+public:
+	Cow()
+	{
+		cout << "Cow 생성자 호출" << endl;
+	}
+
+	void makesound()
+	{
+		cout << "Cow: Mooooo" << endl;
+	}
+
+	~Cow()
+	{
+		cout << "Cow 소멸자 호출" << endl;
+	}
+
+private:
+
+};
+
+int main()
+{
+	Animal* myAnimals[3];
+	myAnimals[0] = new Dog();
+	myAnimals[1] = new Cat();
+	myAnimals[2] = new Cow();
+	
+	for (int i = 0; i < 3; i++)
+	{
+		myAnimals[i]->makesound();
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		delete myAnimals[i];
+	}
+	return 0;
+}
